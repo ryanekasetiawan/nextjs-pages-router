912aa7ae62dabb45f1254140783520055dbe22e7
ryan.goodday123 <ryan.goodday123@gmail.com>
2024-08-09 10:43:15 +0000

    Setup Next-Auth #1

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,9 +1,11 @@
+import { signIn } from "next-auth/react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}> 
       <div className="big">Navbar</div>
+      <button onClick={() => signIn()}>Sign in</button>
     </div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import AppShell from "../components/layouts/AppShell";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <AppShell>
-      <Component {...pageProps} />
-    </AppShell>
-  )
+    <SessionProvider session={session}>
+      <AppShell>
+        <Component {...pageProps} />
+      </AppShell>
+    </SessionProvider>
+  );
 }
