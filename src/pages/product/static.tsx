90b144eb9c70be6afbcbf759bd0189eee01c3e01
ryan.goodday123 <ryan.goodday123@gmail.com>
2024-08-09 03:08:55 +0000

    Static Site Generation

---
+++ src/pages/product/static.tsx
@@ -0,0 +1,25 @@
+import { ProductType } from "@/types/product.type";
+import ProductView from "@/views/Product";
+
+const ProductPage = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  return (
+    <div>
+      <ProductView products={products} />
+    </div>
+  );
+};
+
+export default ProductPage;
+
+export async function getStaticProps() {
+  //fetch data
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
