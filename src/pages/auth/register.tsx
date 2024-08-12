03c0348ee66e5b837bdbe2676bc215a846a2a9a4
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-08-12 15:04:24 +0700

    Auth Register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,14 +1,12 @@
+import RegisterView from "@/views/Auth/Register";
 import Link from "next/link";
 
 const RegisterPage = () => {
-    return (
-        <div>
-            <h1>Register Page</h1>
-            <p>
-                Sudah punya akun? login <Link href={"/auth/login"}>disini</Link>
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <RegisterView />
+    </div>
+  );
 };
 
 export default RegisterPage;
---
+++ src/views/Auth/Register/index.tsx
@@ -0,0 +1,99 @@
+import Link from "next/link";
+import styles from "./Register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const RegisterView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { push } = useRouter();
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const data = {
+      email: event.target.email.value,
+      fullname: event.target.fullname.value,
+      password: event.target.password.value,
+    };
+    const result = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await result.json();
+
+    if (result.status === 200) {
+      event.target.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(result.status === 400 ? "Email already exists" : "");
+    }
+  };
+  return (
+    <div className={styles.register}>
+      <h1 className={styles.register__title}>Register</h1>
+      <div className={styles.register__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.register__form__item}>
+            <label
+              htmlFor="email"
+              className={styles.register__form__item__label}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email"
+              className={styles.register__form__item__input}
+            />
+          </div>
+          <div className={styles.register__form__item}>
+            <label
+              htmlFor="fullname"
+              className={styles.register__form__item__label}
+            >
+              Fullname
+            </label>
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              placeholder="fullname"
+              className={styles.register__form__item__input}
+            />
+          </div>
+          <div className={styles.register__form__item}>
+            <label
+              htmlFor="password"
+              className={styles.register__form__item__label}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+              className={styles.register__form__item__input}
+            />
+          </div>
+          <button type="submit" className={styles.register__form__item__button}>
+            Register
+          </button>
+        </form>
+      </div>
+      <p className={styles.register__link}>
+        Have an account? Sign in <Link href="/auth/login">here</Link>
+      </p>
+    </div>
+  );
+};
+
+export default RegisterView;
