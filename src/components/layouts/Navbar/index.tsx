bfab84dd081d3af10c806aa38c3103a59c9df4d7
ryan.goodday123 <ryan.goodday123@gmail.com>
2024-08-13 08:01:56 +0000

    Login Google #3

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -6,7 +6,14 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div className="big">Navbar</div>
-      <div>
+      <div className={styles.profile}>
+        {data.user.image && (
+          <img
+            className={styles.avatar}
+            src={data.user.image}
+            alt={data.user.fullname}
+          />
+        )}
         {data && data.user.fullname}{" "}
         {data ? (
           <button className={styles.button} onClick={() => signOut()}>
