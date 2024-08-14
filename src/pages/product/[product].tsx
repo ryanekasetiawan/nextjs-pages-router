b5e898c01069f7e2e75ed35470c6350ea6622580
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-08-14 10:44:05 +0000

    Change ENV API

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -25,7 +25,43 @@ const DetailProductPage = ({ product }: { product: ProductType }) => {
 export default DetailProductPage;
 
 // server-side rendering
-// export async function getServerSideProps({
+export async function getServerSideProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  //fetch data
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/product/${params.product}`
+  );
+  const response = await res.json();
+
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
+
+// static-site generation
+// fungsi ini digunakan untuk mendapatkan daftar semua produk yang ada di database
+// dan mengembalikan path yang dapat digunakan untuk static generation
+// export async function getStaticPaths() {
+//   const res = await fetch("http://localhost:3000/api/product");
+//   const response = await res.json();
+
+//   const paths = response.data.map((product: ProductType) => ({
+//     params: {
+//       product: product.id,
+//     },
+//   }));
+
+//   return { paths, fallback: false };
+// }
+
+// Fungsi getStaticProps digunakan untuk mendapatkan data secara statis
+// Fungsi ini akan dipanggil setiap kali saat halaman diload secara statis
+// export async function getStaticProps({
 //   params,
 // }: {
 //   params: { product: string };
@@ -42,39 +78,3 @@ export default DetailProductPage;
 //     },
 //   };
 // }
-
-// static-site generation
-// fungsi ini digunakan untuk mendapatkan daftar semua produk yang ada di database
-// dan mengembalikan path yang dapat digunakan untuk static generation
-export async function getStaticPaths() {
-  const res = await fetch("http://localhost:3000/api/product");
-  const response = await res.json();
-
-  const paths = response.data.map((product: ProductType) => ({
-    params: {
-      product: product.id,
-    },
-  }));
-
-  return { paths, fallback: false };
-}
-
-// Fungsi getStaticProps digunakan untuk mendapatkan data secara statis
-// Fungsi ini akan dipanggil setiap kali saat halaman diload secara statis
-export async function getStaticProps({
-  params,
-}: {
-  params: { product: string };
-}) {
-  //fetch data
-  const res = await fetch(
-    `http://localhost:3000/api/product/${params.product}`
-  );
-  const response = await res.json();
-
-  return {
-    props: {
-      product: response.data,
-    },
-  };
-}
