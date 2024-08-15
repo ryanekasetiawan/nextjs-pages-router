505117695a059a9b9fe2353cfc8204c1c98964d4
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-08-15 14:09:42 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,19 +4,22 @@
     pkgs.nodejs_20
   ];
   idx.extensions = [
-    
-  
- "anseki.vscode-color"
- "christian-kohler.path-intellisense"
- "Codeium.codeium"
- "dbaeumer.vscode-eslint"
- "dsznajder.es7-react-js-snippets"
- "esbenp.prettier-vscode"
- "formulahendry.auto-rename-tag"
- "mhutchie.git-graph"
- "mrmlnc.vscode-scss"
- "oderwat.indent-rainbow"
- "steoates.autoimport"];
+    "anseki.vscode-color"
+    "christian-kohler.path-intellisense"
+    "Codeium.codeium"
+    "dbaeumer.vscode-eslint"
+    "dsznajder.es7-react-js-snippets"
+    "esbenp.prettier-vscode"
+    "formulahendry.auto-rename-tag"
+    "mhutchie.git-graph"
+    "mrmlnc.vscode-scss"
+    "oderwat.indent-rainbow"
+    "steoates.autoimport"
+    "bradlc.vscode-tailwindcss"
+    "ms-vscode.js-debug"
+    "ms-vscode.vscode-typescript-next"
+    "rangav.vscode-thunder-client"
+    "Zignd.html-css-class-completion"];
   idx.previews = {
     previews = {
       web = {
